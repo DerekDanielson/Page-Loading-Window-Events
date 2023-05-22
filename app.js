d578eed108daa9d82ce7d3cbbf67116429b4830b
Derek <derekdanielson84@gmail.com>
2023-05-22 11:33:27 -0600

    Notes added

--- app.js
+++ app.js
@@ -15,3 +15,15 @@ window.addEventListener('scroll', () => {
         document.body.style.color = 'black';
     }
 });
+
+window.addEventListener('focus', () => {
+    document.querySelectorAll('p').forEach((p) => {
+        p.style.color = 'blue';
+    });
+});
+
+window.addEventListener('blur', () => {
+    document.querySelectorAll('p').forEach((p) => {
+        p.style.color = 'black';
+    });
+});
